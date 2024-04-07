23b9ac65848d0e32d6cfd983ee90204ee6375c29
Tejender Kumar <galactus@Tejenders-MacBook-Pro.local>
2024-04-07 13:14:46 +0530

    added menu

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,113 +1,120 @@
-import Image from "next/image";
+'use client'
+import React, { useState, useRef } from 'react';
+import { useQRCode } from 'next-qrcode';
+import html2canvas from 'html2canvas';
 
-export default function Home() {
-  return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">src/app/page.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{" "}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
-      </div>
+export default function QrGenerator() {
 
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-full sm:before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-full sm:after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
+  const [selectedColor, setSelectedColor] = useState('default'); // Add a state for color choice
+
+  const defaultColors: any = {
+    default: { dark: '#010599FF', light: '#FFFFFF' }, // Your original theme
+    theme1: { dark: '#C1121F', light: '#FFFFFF' }, 
+    theme2: { dark: '#364F6B', light: '#FFFFFF' },
+  
+    // New themes:
+    theme3: { dark: '#374151', light: '#FFFFFF'}, // Monochrome sophistication
+    theme4: { dark: '#A93F55', light: '#FFFFFF'}, // Warm and vintage
+    theme5: { dark: '#23815D', light: '#FFFFFF'}, // Nature inspired
+    theme6: { dark: '#132C49', light: '#FFFFFF'}, // Cool and modern
+    theme7: { dark: '#6236FF', light: '#FFFFFF'}, // Vibrant purple shades
+    theme8: { dark: '#DC2626', light: '#FFFFFF'}, // Bold red with softness
+    theme9: { dark: '#D97706', light: '#FFFFFF'}, // Earthy orange + cream
+    theme10: { dark: '#10B981', light: '#FFFFFF'} // Bright green and neutral
+  };
+  const [menuUrl, setMenuUrl] = useState('');
+  const [qrCode, setQrCode] = useState<any>();
+  const qrCodeRef = useRef(null);
+  const [isButtonDisabled, setIsButtonDisabled] = useState(true); // State to control button state
+
+
+  const handleInputChange = (event:any) => {
+    setMenuUrl(event.target.value);
+    setIsButtonDisabled(event.target.value.trim() === ''); // Update button state
+  
+  };
+
+  const { Image } = useQRCode();
+  const generateQRCode = async () => {
+    try {
+      const qr = (
+        <Image 
+          text={menuUrl}
+          options={{
+            type: 'image/jpeg',
+            quality: 0.9,
+            width:300,
+            margin: 2,
+            color:defaultColors[selectedColor],
+          }}
         />
-      </div>
+      );
+      setQrCode(qr);
+    } catch (error) {
+      console.error(error);
+    }
+  };
 
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
+  const downloadQRCode = async () => {
+    if (qrCodeRef.current) {
+      const canvas = await html2canvas(qrCodeRef.current); 
+      const imageData = canvas.toDataURL('image/png');
 
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
+      const link = document.createElement('a');
+      link.download = 'menu-qrcode.png';
+      link.href = imageData;
+      link.click(); 
+    }
+  };
 
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore starter templates for Next.js.
-          </p>
-        </a>
+  return (
+    <div className='bg-blue-300 w-[100%] m-auto h-[100vh] flex flex-col items-center justify-center'>
+        <p className='text-md lg:w-[30%] p-3 text-center full mx-auto leading-loose tracking-wider
+         italic'>
+          This app let you create a qr code containg a web address where you want to 
+          land your user / person scanning that qr code.<code></code></p>
+          <p className='px-3 text-center underline text-xs my-2'>Enter the url, choose the theme and click on generate qr code and download</p>
+        <div className='bg-teal-400 h-fit w-fit lg:px-20 lg:py-10 py-10 px-5 rounded-lg flex flex-col gap-4 justify-center items-center'>
+        <h2 className='font-bold text-4xl text-blue-950'>QR Code Generator</h2>
+          <div className='flex flex-col gap-3'>
+          <div className='flex flex-col gap-2 h-auto '>
+            <label htmlFor="url" className='text-slate-600 font-bold'>Enter Website Address</label>
+            <input className=' rounded-md text text-slate-600 h-10 border-2 p-2 w-full border-blue-400 '
+              type="text"
+              id='url'
+              placeholder="Example : https://www.menu.vercel.app/"
+              value={menuUrl}
+              onChange={handleInputChange}
+              required
+          />
+          </div >
+          <div className='flex flex-col gap-2'> {/* Wrapper for color selectors */}
+          <span className='text-slate-600 font-bold'>Choose Qr Theme</span> 
+          <div className='flex gap-2'>
+              {Object.keys(defaultColors).map((themeName) => (
+                <button
+                  key={themeName}
+                  className={`w-6 h-6   ${selectedColor === themeName ? 'border-2 border-black' : ''}`} // Mark selected
+                  style={{ backgroundColor: defaultColors[themeName].dark }}
+                  onClick={() => setSelectedColor(themeName)} 
+                ></button>
+              ))}
 
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50 text-balance`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
+          </div>
+            </div>
+            </div>
+
+        
+        
+        <button disabled={isButtonDisabled} onClick={generateQRCode} className='bg-yellow-200 text-black p-3 rounded-lg'>Generate QR Code</button>
+       
+        {qrCode &&( <div ref={qrCodeRef} className='bg-red-300 p-2 w-fit h-fit rounded-md'> 
+            {qrCode}
+        </div>)}
+        {qrCode && (
+            <button onClick={downloadQRCode} className='bg-slate-600 p-3 rounded-lg shadow-white shadow-md shadow-stone-400'>Download</button>
+        )}
+        </div>
+    </div>
   );
 }
