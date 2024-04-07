7bb3989f28fab6c13808e700889a9251f6f92e23
Tejender Kumar <galactus@Tejenders-MacBook-Pro.local>
2024-04-07 19:35:55 +0530

    addded new ui

--- src/app/menu/winterfell-treehouse/page.tsx
+++ src/app/menu/winterfell-treehouse/page.tsx
@@ -34,8 +34,8 @@ export default function Page() {
         {
           category: "Shakes",
           items: [
-            { name: "Banana", price: 120, thumbnail: "/images/banana-shake.jpg" },
-            { name: "Mango", price: 120, thumbnail: "/images/mango-shake.jpg" },
+            { name: "Banana Shake", price: 120, thumbnail: "/images/banana-shake.jpg" },
+            { name: "Mango Shake", price: 120, thumbnail: "/images/mango-shake.jpg" },
           ]
         },
       
@@ -56,9 +56,9 @@ export default function Page() {
         {
           category: "Omlates",
           items: [
-            { name: "Plain Omlt.", price: 50, thumbnail: "/images/plain-omlette.jpg" },
-            { name: "Bread Omlt.", price: 100, thumbnail: "/images/bread-omlette.jpg" },
-            { name: "Cheese Omlt.", price: 150, thumbnail: "/images/cheese-omlette.jpg" },
+            { name: "Plain Omlatte", price: 50, thumbnail: "/images/plain-omlette.jpg" },
+            { name: "Bread Omlatte", price: 100, thumbnail: "/images/bread-omlette.jpg" },
+            { name: "Cheese Omlatte", price: 150, thumbnail: "/images/cheese-omlette.jpg" },
           ]
         },
       
@@ -66,9 +66,9 @@ export default function Page() {
         {
           category: "Tosts",
           items: [
-            { name: "Plain", price: 20, thumbnail: "/images/plain-toast.jpg" },
-            { name: "Butter", price: 40, thumbnail: "/images/butter-toast.jpg" },
-            { name: "Honey", price: 50, thumbnail: "/images/honey-toast.jpg" },
+            { name: "Plain toast", price: 20, thumbnail: "/images/plain-toast.jpg" },
+            { name: "Butter toast", price: 40, thumbnail: "/images/butter-toast.jpg" },
+            { name: "Honey toast", price: 50, thumbnail: "/images/honey-toast.jpg" },
           ]
         },
       
@@ -76,10 +76,10 @@ export default function Page() {
         {
           category: "Sandwich",
           items: [
-            { name: "Veg", price: 100, thumbnail: "/images/veg-sandwich.jpg" },
-            { name: "Chicken", price: 150, thumbnail: "/images/chicken-sandwich.jpg" },
-            { name: "Egg", price: 120, thumbnail: "/images/egg-sandwich.jpg" },
-            { name: "Cheese", price: 150, thumbnail: "/images/cheese-sandwich.jpg" },
+            { name: "Veg Sandwich", price: 100, thumbnail: "/images/veg-sandwich.jpg" },
+            { name: "Chicken Sandwich", price: 150, thumbnail: "/images/chicken-sandwich.jpg" },
+            { name: "Egg Sandwich", price: 120, thumbnail: "/images/egg-sandwich.jpg" },
+            { name: "Cheese Sandwich", price: 150, thumbnail: "/images/cheese-sandwich.jpg" },
           ]
         },
       
@@ -94,30 +94,62 @@ export default function Page() {
     }, []);
     // Filtered menu data based on selected category
     const filteredMenuData = selectedCategory === 'All' ? menuData : menuData.filter(category => category.category === selectedCategory);
-
+   
     return (
-        <div className="menu-container flex flex-col gap-2">
-            <div className='flex flex-col gap-2 justify-center items-center
-     text-center bg-slate-950 text-white p-5 font-serif tracking-widest rounded-lg'>
+        <div className="menu-container flex flex-col gap-2 mb-16">
+            <div className='h-[200px] w-[200px] mx-auto flex flex-col gap-2 justify-center items-center
+     text-center bg-slate-950 text-white p-5 font-serif tracking-widest rounded-full z-20
+     border-8 border-white'>
         <div className='w-20 h-20 mx-auto   p-1 bg-white rounded-full'>
             <Image src='/images/winterfell/winterfell.jpeg' alt='winterfell logo'
             height={100} width={100} style={{height:'100%' , width:'100%',objectFit:'cover'}}
             className='rounded-full'/>
         </div>
-        <h1 className='text-4xl uppercase  '>Food Menu</h1>
-        <h2>Winterfell Treehouse Jibhi</h2>
+        <h1 className='text-xl uppercase  '>Food Menu</h1>
+        <h2 className='text-xs mx-5'>Winterfell Treehouse </h2>
     </div>
 
-            <section className="category-section sticky top-0 bg-white rounded-lg">
-                
-                   
-                <ul
+            {/* Display filtered menu items */}
+            <div className='bg-slate-200 w-full pt-20 -mt-24 rounded-3xl'>
+
+{filteredMenuData.map((category) => (
+    <section key={category.category} className=' px-2 flex flex-col gap-3 py-5'>
+        <h2 className='bold text-xl font-serif text-center'>{category.category}</h2>
+        <div className="item-grid flex flex-wrap gap-4 ">
+            {category.items.map((item, index) => (
+                <div
+                    className={`item-card ${category.items.length % 2 !== 0 && index === category.items.length - 1 ?
+                         'w-[98%] mx-auto h-[150px] flex-row gap-5 justify-start ' : 'w-[47%] flex-col h-[200px] justify-center'}
+                      flex  items-center  gap-4 bg-gray-50 p-1  rounded-md`}
+                    key={item.name}
+                >
+                    <div className='h-20 w-20'>
+                        <Image
+                            src='https://cdn.pixabay.com/photo/2016/10/13/11/44/chocolates-1737503_1280.jpg'
+                            alt={item.name}
+                            height={60}
+                            width={60}
+                            style={{ width: '100%', height: '100%', objectFit: 'cover' }}
+                            className='rounded-full'
+                        />
+                    </div>
+                    <div>
+                        <h3 className='font-bold'>{item.name}</h3>
+                        <p>₹{item.price}</p>
+                    </div>
+                </div>
+            ))}
+        </div>
+    </section>
+))}
+
+            </div>
+
+<section className=" w-full fixed bottom-0 left-0 category-section   bg-white rounded-lg ">
+ <ul
+   className='overflow-scroll flex  gap-3 p-5 w-[95%] mx-auto text-sm border-b border-blue-950 '>
         
-         className=' flex overflow-scroll gap-3 p-3 w-[95%] mx-auto text-sm border-b border-blue-950'
-        
-      >
-        
-        <li className={`min-w-fit bg-teal-100 p-2 rounded-md ${selectedCategory === 'All' ? 'bg-teal-500 text-white' : ''}`}
+        <li className={`min-w-fit  p-2 rounded-full border  border-[#FEBD2D] ${selectedCategory === 'All' ? 'bg-[#FEBD2D] text-white' : ''}`}
            onClick={() => setSelectedCategory('All')}>
              All 
                         </li>
@@ -126,7 +158,7 @@ export default function Page() {
                         [...categories].map(category => (
                             <li
                                 key={category}
-                                className={`border  border-slate-600 min-w-fit w-20  p-2 rounded-md ${selectedCategory === category ? 'bg-teal-500 text-white' : ''}`}
+                                className={`border  border-[#FEBD2D] min-w-fit w-20  p-2 rounded-full ${selectedCategory === category ? 'bg-[#FEBD2D] text-white' : ''}`}
                                 onClick={() => setSelectedCategory(category)}
                             >
                                 {category}
@@ -137,33 +169,6 @@ export default function Page() {
       
                 </ul>
             </section>
-
-            {/* Display filtered menu items */}
-            {filteredMenuData.map((category) => (
-                <section key={category.category} className='bg-gray-100 px-4 flex flex-col gap-3 py-5'>
-                    <h2 className='bold text-3xl'>{category.category}</h2>
-                    <div className="item-grid flex flex-col gap-4 ">
-                        {category.items.map((item) => (
-                            <div className="item-card flex gap-4 bg-teal-300 p-4 rounded-md" key={item.name}>
-                                <div className='h-16 w-16'>
-                                    <Image
-                                        src='https://cdn.pixabay.com/photo/2016/10/13/11/44/chocolates-1737503_1280.jpg'
-                                        alt={item.name}
-                                        height={60}
-                                        width={60}
-                                        style={{ width: '100%', height: '100%', objectFit: 'cover' }}
-                                        className='rounded-full'
-                                    />
-                                </div>
-                                <div>
-                                    <h3 className='font-bold'>{item.name}</h3>
-                                    <p>₹{item.price}</p>
-                                </div>
-                            </div>
-                        ))}
-                    </div>
-                </section>
-            ))}
         </div>
     );
 }
